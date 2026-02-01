886e133ffee8ca14821c4f9172af58d7439eec50
tikvi4ka <eknedeva@edu.aau.at>
2026-02-01 18:29:31 +0100

    :changed the backbones for variety :)

--- model.py
+++ model.py
@@ -7,15 +7,15 @@ class TransferModel(nn.Module):
     def __init__(
         self,
         num_classes: int,
-        backbone: str = "resnet18",
+        backbone: str = "densenet121",
         pretrained: bool = True,
         dropout: float = 0.2,
     ):
         super().__init__()
         self.backbone_name = backbone
 
-        if backbone == "resnet18":
-            weights = models.ResNet18_Weights.DEFAULT if pretrained else None
+        if backbone == "densenet121":
+            weights = models.DenseNet121_Weights.DEFAULT if pretrained else None
             self.model = models.resnet18(weights=weights)
 
             feat_dim = self.model.fc.in_features
@@ -24,8 +24,8 @@ class TransferModel(nn.Module):
                 nn.Linear(feat_dim, num_classes),
             )
 
-        elif backbone == "mobilenet_v2":
-            weights = models.MobileNet_V2_Weights.DEFAULT if pretrained else None
+        elif backbone == "efficientnet_b0":
+            weights = models.EfficientNet_B0_Weights.DEFAULT if pretrained else None
             self.model = models.mobilenet_v2(weights=weights)
 
             feat_dim = self.model.classifier[1].in_features
@@ -34,7 +34,7 @@ class TransferModel(nn.Module):
                 nn.Linear(feat_dim, num_classes),
             )
         else:
-            raise ValueError("backbone must be 'resnet18' or 'mobilenet_v2'.")
+            raise ValueError("backbone must be 'densenet121' or 'efficientnet_b0'")
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.model(x)
@@ -49,18 +49,20 @@ class TransferModel(nn.Module):
 
     def train_head_only(self) -> None:
         self.freeze_all()
-        head = self.model.fc if self.backbone_name == "resnet18" else self.model.classifier
-        for p in head.parameters():
+        for p in self.model.classifier.parameters():
             p.requires_grad = True
 
     def fine_tune_last_block(self) -> None:
         self.freeze_all()
-        if self.backbone_name == "resnet18":
-            for p in self.model.layer4.parameters():
+
+        if self.backbone_name == "densenet121":
+            for p in self.model.features.denseblock4.parameters():
                 p.requires_grad = True
-            for p in self.model.fc.parameters():
+            for p in self.model.features.norm5.parameters():
                 p.requires_grad = True
-        else:
+            for p in self.model.classifier.parameters():
+                p.requires_grad = True
+        else:  #efficientnet_b0
             for p in self.model.features[-3:].parameters():
                 p.requires_grad = True
             for p in self.model.classifier.parameters():
